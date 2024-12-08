acb06c4fb08b2ebc3c0034f26bbc38990935b860
rugmj <rugmj@rugmj.dev>
2024-12-08 00:22:08 +0000

    2.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# Rust led server
+> Led server written in rust
+
+## Setup
+
+### Pi zero w 1
+
+1. Enable spi
+2. Set spi buf siz to 8192
+    `/etc/modprobe.d/spidev.conf`
+    ```conf
+    options spidev bufsiz=8192
+    ```
+3. Set core clock speed to 250
+    `/boot/config.txt`
+    ```conf
+    # Snip
+    core_freq=250
+    # Snip
+    ```
+
+## Usage
+
+`rust-led-server {number of leds}`
+
+### Example
+
+`rust-led-server 170`
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use sacn_unofficial::packet::ACN_SDT_MULTICAST_PORT;
-use sacn_unofficial::receive::SacnReceiver;
+use sacn_unofficial::{packet::ACN_SDT_MULTICAST_PORT, receive::SacnReceiver};
 
-use std::env;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::time::Duration;
+use std::{
+    env,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    time::Duration,
+};
 
-use ws2818_rgb_led_spi_driver::adapter_gen::WS28xxAdapter;
-use ws2818_rgb_led_spi_driver::adapter_spi::WS28xxSpiAdapter;
-use ws2818_rgb_led_spi_driver::encoding::encode_rgb;
+use ws2818_rgb_led_spi_driver::{
+    adapter_gen::WS28xxAdapter, adapter_spi::WS28xxSpiAdapter, encoding::encode_rgb,
+};
 
 const UNIVERSE1: u16 = 1;
 const TIMEOUT: Option<Duration> = Some(Duration::from_secs(30)); // A timeout of None means blocking behaviour, some indicates the actual timeout.
@@ -15,7 +16,8 @@ const TIMEOUT: Option<Duration> = Some(Duration::from_secs(30)); // A timeout of
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let leds = &args[0];
+    let leds = &args[1];
+    let leds: usize = leds.trim().parse().expect("# of Leds wasn't a number.");
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), ACN_SDT_MULTICAST_PORT);
 
@@ -26,18 +28,29 @@ fn main() {
     let mut adapter = WS28xxSpiAdapter::new("/dev/spidev0.0").unwrap();
 
     loop {
-        // .recv(TIMEOUT) handles processing synchronised as-well as normal data.
         match dmx_rcv.recv(TIMEOUT) {
             Err(e) => {
-                println!("{:?}", e);
+                eprintln!("{:?}", e);
             }
             Ok(p) => {
                 let mut spi_encoded_rgb_bits = vec![];
-                for n in 0..leds.trim().parse().expect("# of Leds wasn't a number.") {
+                if let None = p.first() {
+                    continue;
+                }
+
+                let values = &p[0].values;
+
+                if values.len() != leds * 3 {
+                    eprintln!("Received data length is {} not {}", values.len(), leds * 3);
+
+                    continue;
+                }
+
+                for n in 0..leds {
                     let i = n * 3;
-                    let r = p[0].values[i + 1];
-                    let g = p[0].values[i + 2];
-                    let b = p[0].values[i];
+                    let r = values[i + 1];
+                    let g = values[i + 2];
+                    let b = values[i];
 
                     spi_encoded_rgb_bits.extend_from_slice(&encode_rgb(r, g, b));
                 }
